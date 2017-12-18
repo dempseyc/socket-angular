fa37d8f16516064ec2539258843f47149537dedf
Craig Dempsey <craigdempsey@gmail.com>
2017-12-18 17:26:04 -0500

    init commit no errors

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,30 @@
 import { Component } from '@angular/core';
+import { SocketService } from '../services/socket.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'app';
+  title = 'VAMPIRE';
+  message: string;
+  messages: string[] = [];
+
+  constructor( private socketService: SocketService) {
+  }
+
+  sendMessage() {
+    this.socketService.sendMessage(this.message);
+    this.message = '';  // just a reset?
+  };
+
+  ngOnInit() {
+    this.socketService
+      .getMessages()
+      .subscribe((message: string) => {
+        this.messages.push(message);
+      });
+  };
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-
 import { AppComponent } from './app.component';
 
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+
+import { SocketService } from '../services/socket.service';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpModule
   ],
-  providers: [],
+  providers: [SocketService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
